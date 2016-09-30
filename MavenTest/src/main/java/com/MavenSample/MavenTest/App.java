f80eaa7bdbc14ee435e059c1ae5148a58fdbb7f6
mitis-ezdi <miti.s@ezdi.us>
2016-09-30 17:03:27 +0530

    Update App.java

--- MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
+++ MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
@@ -14,5 +14,10 @@ public class App
          System.out.println( "Hello World!.....Test2" );
          System.out.println( "Hello World...Test3!" );
         
+        System.out.println( "Hello World!..Test" );
+         System.out.println( "Hello World!.TEst 1" );
+         System.out.println( "Hello World!.....Test2" );
+         System.out.println( "Hello World...Test3!" );
+        
     }
 }

e864222de03160f700207df470c6f45dfe6caa16
mitis-ezdi <miti.s@ezdi.us>
2016-09-30 17:01:08 +0530

    Update App.java

--- MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
+++ MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+         System.out.println( "Hello World!..Test" );
+         System.out.println( "Hello World!.TEst 1" );
+         System.out.println( "Hello World!.....Test2" );
+         System.out.println( "Hello World...Test3!" );
+        
     }
 }

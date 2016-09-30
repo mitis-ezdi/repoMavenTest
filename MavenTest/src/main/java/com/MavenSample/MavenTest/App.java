51332571b285171c1109711d6c3fd524f10a2a0f
mitis-ezdi <miti.s@ezdi.us>
2016-09-30 17:24:33 +0530

    Update App.java

--- MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
+++ MavenTest/src/main/java/com/MavenSample/MavenTest/App.java
@@ -9,15 +9,11 @@ public class App
     public static void main( String[] args )
     {
        
-         System.out.println( "Hello World!..Test" );
+         System.out.println( "Hello World!..Test Change for checking in eclipse" );
          System.out.println( "Hello World!.TEst 1" );
          System.out.println( "Hello World!.....Test2" );
          System.out.println( "Hello World...Test3!" );
         
-        System.out.println( "Hello World!..Test" );
-         System.out.println( "Hello World!.TEst 1" );
-         System.out.println( "Hello World!.....Test2" );
-         System.out.println( "Hello World...Test3!" );
         
     }
 }
